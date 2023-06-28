40d9de589278f28c119e10994977dd840db86156
Aritra <aritraadhikary2003@gmail.com>
2023-06-28 18:40:53 +0530

    Router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Navbar from './Components/Navbar';
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Account from "./Components/Account.js";
+import Login from "./Components/Login.js";
+import Signup from "./Components/Signup.js";
+import About from "./Components/About.js";
 
 function App() {
-  return (
-    <>
-    <Navbar/>
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route exact path="/profile" element={<Account />} />
+				<Route exact path="/login" element={<Login />} />
+				<Route exact path="/signup" element={<Signup />} />
+				<Route exact path="/about" element={<About />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
